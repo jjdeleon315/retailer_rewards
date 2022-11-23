9e4a2c1170f3a71600ffeefa975cb095abc0cba8
Julian De Leon <julian@nirvana.tech>
2022-11-22 22:45:00 -0500

    Implement rewards without transaction repository

---
+++ src/main/java/com/deleon/julian/charter/retailerrewards/RetailerRewardsApplication.java
@@ -0,0 +1,12 @@
+package com.deleon.julian.charter.retailerrewards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailerRewardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailerRewardsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/deleon/julian/charter/retailerrewards/RetailerRewardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deleon.julian.charter.retailerrewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailerRewardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

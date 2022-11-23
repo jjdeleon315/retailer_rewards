e566fef6eadb219a75cc62e7fdb682a3ff83f1b2
Julian De Leon <julian@nirvana.tech>
2022-11-22 20:10:25 -0500

    Add spring init prject

---
+++ src/test/java/com/deleon/julian/retailer/rewards/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deleon.julian.retailer.rewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
